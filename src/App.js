2ce3ec4a1c29467e7399bc33f866f05823fba133
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-07-02 13:04:44 +0200

    Logs and  LogItem added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
-
+import React, { Fragment, useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 import "./App.css";
 
 const App = () => {
@@ -11,9 +13,12 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 };
 
