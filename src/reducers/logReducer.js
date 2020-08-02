a33aaad0aa001335a679ecfec7da2e6b10e9e879
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-08-02 10:05:49 +0200

    search functionality added

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from "../actions/types";
 
 const initialState = {
@@ -42,6 +43,12 @@ export default (state = initialState, action) => {
         logs: state.logs.filter((log) =>
           log.id === action.payload.id ? action.payload : log
         ),
+        loading: false,
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
         loading: false,
       };
     case SET_CURRENT:
